04230cf7d3d904670e54c395847c49a084603424
Gaurav Chauhan <chauhangaurav101@gmail.com>
2018-03-20 00:05:16 +0530

    mongoose connection config

--- routes/fetchAllPost.js
+++ routes/fetchAllPost.js
@@ -4,7 +4,7 @@ var {Blog} = require('../models/blog');
 
 /* GET home page. */
 router.use('/',(req, res, next)=>{
-    console.log(req);
+
     Blog.find().then((posts)=>{
         res.send(posts);
     },(err)=>{
